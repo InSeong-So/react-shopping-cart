3f3cf537ce8bdd520f54f2997d02662311181b66
InSeong-So <goflvhxj2547@gmail.com>
2023-01-21 18:41:28 +0900

    Chore: 개발환경 설정(pnpm + turbo monorepo)

---
+++ packages/web/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { Global } from '@emotion/react';
+
+
+import App from './App';
+
+import reset from './styles/reset';
+import global from './styles/global';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <Global styles={[reset, global]} />
+      <App />
+  </StrictMode>,
+);
