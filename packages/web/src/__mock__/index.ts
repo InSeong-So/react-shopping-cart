59f96c7590936e12873da19858f8e9a04bc4b705
Tene <tene@nextunicorn.kr>
2023-02-01 00:20:16 +0900

    Chore: msw 설정

--- packages/web/src/__mock__/index.ts
+++ packages/web/src/__mock__/index.ts
@@ -2,7 +2,18 @@ import { worker } from './browser';
 import { productStore } from './store';
 
 const initializeMockupWorker = async () => {
-  await Promise.all([productStore.init(), worker.start({ onUnhandledRequest: 'bypass' })]);
+  await Promise.all([
+    productStore.init(),
+    worker.start({
+      onUnhandledRequest: 'bypass',
+      serviceWorker: {
+        url:
+          process.env.NODE_ENV === 'production'
+            ? '/react-shopping-cart/packages/web/public/mockServiceWorker.js'
+            : '/packages/web/public/mockServiceWorker.js',
+      },
+    }),
+  ]);
 };
 
 export default initializeMockupWorker;
